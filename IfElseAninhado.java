689e354c604b166d6453f8dd647a1d3f20a6fe65
antoniocrorusso <56083560+antoniocrorusso@users.noreply.github.com>
2021-03-03 10:25:03 -0300

    If else aninhado

--- IfElseAninhado.java
+++ IfElseAninhado.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class IfElseAninhado {
+import javax.swing.*;
+
+public class IfElseAninhado {
+    public static void main(String[] args) {
+        long nota;
+        nota = Long.parseLong(JOptionPane.showInputDialog("Digite a Nota: "));
+        if (nota >= 90){
+            JOptionPane.showMessageDialog(null, "Parabens");
+            JOptionPane.showMessageDialog(null, "Conceito A");
+        }
+        else {
+            if (nota >= 80){
+                JOptionPane.showMessageDialog(null, "Conceito B");
+            }
+            else {
+                if (nota >= 70){
+                    JOptionPane.showMessageDialog(null, "Conceito C");
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, "Reprovado");
+                }
+            }
+        }
+    }
 }
