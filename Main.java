b5bdd1ca211fd299c490dbeeee6415594340bba0
antoniocrorusso <56083560+antoniocrorusso@users.noreply.github.com>
2021-03-03 10:59:45 -0300

    exercício URI

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int A, B, C, D;
+        Scanner leitor = new Scanner(System.in);
+        A = leitor.nextInt();
+        B = leitor.nextInt();
+        C = leitor.nextInt();
+        D = leitor.nextInt();
+        if (B > C && D > A && C + D > A + B && C >= 0 && D >= 0 && A % 2 == 0) {
+            // print ("Valores aceitos")
+            System.out.print("Valores aceitos");
+        }
+        else {
+            System.out.println("Valores nao aceitos");
+        }
+    }
+}
